831c61c3f20240af0fbebc7772d3044d01417367
deepg3052-debug <deepg3052@gmail.com>
2026-05-09 14:56:28 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,10 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-const { protect } = require('../middleware/authMiddleware');
+/* ====================================
+   IMPORT MIDDLEWARE
+==================================== */
+
+const {
+
+    protect
+
+} = require('../middleware/authMiddleware');
 
 /* ====================================
-   IMPORT CONTROLLER
+   IMPORT CONTROLLERS
 ==================================== */
 
 const {
@@ -24,25 +32,61 @@ const {
    GET ALL PAYMENTS
 ==================================== */
 
-router.get('/', protect, getPayments);
+// PROTECTED ROUTE
+
+router.get(
+
+    '/',
+
+    protect,
+
+    getPayments
+
+);
 
 /* ====================================
    CREATE PAYMENT
 ==================================== */
 
-router.post('/', protect, createPayment);
+// PROTECTED ROUTE
+
+router.post(
+
+    '/',
+
+    protect,
+
+    createPayment
+
+);
 
 /* ====================================
    UPDATE PAYMENT
 ==================================== */
 
-router.put('/:id', protect, updatePayment);
+router.put(
+
+    '/:id',
+
+    protect,
+
+    updatePayment
+
+);
 
 /* ====================================
    DELETE PAYMENT
 ==================================== */
 
-router.delete('/:id', protect, deletePayment);
+router.delete(
+
+    '/:id',
+
+    protect,
+
+    deletePayment
+
+);
 
 /* ====================================
    EXPORT ROUTER
